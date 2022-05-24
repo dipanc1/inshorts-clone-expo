7dd7df887c1d05f6ce50a8bba1ffa1af2e07a1d2
dipanc1 <dipanchhabra@gmail.com>
2022-05-24 17:02:34 +0530

    yes

--- components/TopNavigation.js
+++ components/TopNavigation.js
@@ -1,24 +1,53 @@
-import { View, TouchableOpacity, Text } from 'react-native'
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
 import React from 'react'
-import { } from 'react-native-web';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign, MaterialCommunityIcons, SimpleLineIcons } from '@expo/vector-icons';
 
 const TopNavigation = ({ index, setIndex }) => {
   return (
     <View style={{ ...styles.container, backgroundColor: '#282c35' }}>
       {index === 0 ?
-        <TouchableOpacity style={styles.left}>
-          <Text style={{ ...styles.text, color: '#ffc107' }}>
-            <MaterialCommunityIcons
-              name='theme-light-dark'
-              size={30}
+        (
+          <TouchableOpacity style={styles.left}>
+            <Text style={{ ...styles.text, color: '#ffc107' }}>
+              <MaterialCommunityIcons
+                name='theme-light-dark'
+                size={24}
+                color='#ffc107'
+              />
+            </Text>
+          </TouchableOpacity>
+        )
+        : (
+          <TouchableOpacity
+            style={styles.left}
+            onPress={() => setIndex(index === 0 ? 1 : 0)}
+          >
+            <SimpleLineIcons
+              name='arrow-left'
+              size={15}
               color='#ffc107'
             />
+            <Text style={{ ...styles.text, color: 'lightgrey' }}>Discover</Text>
+          </TouchableOpacity>
+        )}
+
+      <Text style={{ ...styles.center, color: 'white' }}>
+        {index ? 'All News' : 'Discover'}
+      </Text>
+      {index ?
+        (<TouchableOpacity style={styles.right}>
+          <Text style={styles.text}>
+            <AntDesign name='reload1' size={24} color='#ffc107' />
           </Text>
-        </TouchableOpacity>
-        : <>
-        </>
-      }
+        </TouchableOpacity>)
+        : (<TouchableOpacity style={styles.left}
+          onPress={() => setIndex(index === 0 ? 1 : 0)}
+        >
+          <Text style={{ ...styles.text, color: 'white' }}>
+            All News
+          </Text>
+          <SimpleLineIcons name='arrow-right' size={15} color='#ffc107' />
+        </TouchableOpacity>)}
     </View>
   )
 };
@@ -32,14 +61,26 @@ const styles = StyleSheet.create({
     borderBottomWidth: 0.5,
     borderBottomColor: 'black',
   },
-  left:{
+  left: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     width: 80,
   },
-  text:{
+  right: {
+    alignItems: 'flex-end',
+    width: 80,
+  },
+  text: {
     fontSize: 20,
+  },
+  center: {
+    fontSize: 16,
+    fontWeight: '700',
+    borderBottomColor: '#ffc107',
+    borderBottomWidth:5,
+    borderRadius: 10,
+    paddingBottom: 6,
   }
 });
 
